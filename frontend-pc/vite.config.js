09955671eeabc0b1ea0868293206c7e47d733785
hu.po <hugo.p.cmu@gmail.com>
2024-10-23 14:24:33 -0500

    asd

--- frontend-pc/vite.config.js
+++ frontend-pc/vite.config.js
@@ -4,8 +4,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    host: '0.0.0.0',
     port: 3000,
+    strictPort: true,
+    hmr: {
+      clientPort: 3000,
+      host: '0.0.0.0',
+    },
+    watch: {
+      usePolling: true,
+    },
     proxy: {
       '/api': {
         target: process.env.BACKEND_URL,
