d84174372455c7d61b1ceef093e142a37695bbf4
hu.po <hugo.p.cmu@gmail.com>
2024-10-29 11:15:03 -0500

    asd

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -6,11 +6,12 @@ if [ "$(docker ps -a -q)" ]; then
 else
   echo "No running containers to stop."
 fi
-docker system prune -f
+
 docker rmi -f aquarius-frontend-pc aquarius-backend aquarius-frontend-vr
 docker image prune -f
 docker volume prune -f
 docker network prune -f
+# docker system prune -f
 
 rm -f .env
 rm -rf data
