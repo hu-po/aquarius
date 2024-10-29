b2a251cf1764eda714cae7a459ef42ddbeb1bbae
hu.po <hugo.p.cmu@gmail.com>
2024-10-29 11:08:26 -0500

    asd

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 echo "🧹 cleaning aquarius ..."
-docker stop $(docker ps -q)
-docker rmi aquarius-frontend-pc aquarius-backend aquarius-frontend-vr
+
+if [ "$(docker ps -q)" ]; then
+  docker stop $(docker ps -q)
+else
+  echo "No running containers to stop."
+fi
+
+docker rmi -f aquarius-frontend-pc aquarius-backend aquarius-frontend-vr
 docker image prune -f
 docker volume prune -f
 docker network prune -f
+
 rm -f .env
 rm -rf data
+
 git pull
