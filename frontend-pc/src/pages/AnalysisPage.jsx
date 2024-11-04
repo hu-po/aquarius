35a542e2a329465a5ed0f925a4103b279188f9ae
hu.po <hugo.p.cmu@gmail.com>
2024-11-04 12:02:33 -0600

    asd

---
+++ frontend-pc/src/pages/AnalysisPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AIAnalysis } from '../components';
+
+export const AnalysisPage = () => {
+  return (
+    <div className="analysis-page">
+      <AIAnalysis />
+    </div>
+  );
+};
+
+export default AnalysisPage; 
