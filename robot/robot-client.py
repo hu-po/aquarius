fc9424a93b43ac922795edf855cdbf5f5d552438
hu.po <hugo.p.cmu@gmail.com>
2024-11-04 07:25:40 -0600

    qwe

---
+++ robot/robot-client.py
@@ -0,0 +1,4 @@
+# robot client runs inside a docker container on the agx orin
+# robot server runs on the bare metal of the robot arm
+
+# Maximum KISS principle, the only function we need of the pymycobot library is to send angles to the robot arm
