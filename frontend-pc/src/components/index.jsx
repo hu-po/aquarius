25798fff02c122fb89d325cd86521a165e1b40fd
hu.po <hugo.p.cmu@gmail.com>
2024-11-06 10:16:09 -0600

    qwe

--- frontend-pc/src/components/index.jsx
+++ frontend-pc/src/components/index.jsx
@@ -3,3 +3,4 @@ export { default as AIAnalysis } from './AIAnalysis';
 export { default as Stats } from './Stats';
 export { default as CameraStream } from './CameraStream';
 export { default as Life } from './Life';
+export { default as TrajectoryBrowser } from './TrajectoryBrowser';
