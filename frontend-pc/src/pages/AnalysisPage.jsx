2637f4bbfd5dc8af64e1ae644cc8999f6db27b26
hu.po <hugo.p.cmu@gmail.com>
2024-11-12 16:12:08 -0600

    qwe

--- frontend-pc/src/pages/AnalysisPage.jsx
+++ frontend-pc/src/pages/AnalysisPage.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AnalysisControl } from '../components';
 import AnalysisHistory from '../components/AnalysisHistory';
 
 export const AnalysisPage = () => {
+  const [analysisComplete, setAnalysisComplete] = useState(false);
+
+  const handleAnalysisComplete = (results) => {
+    setAnalysisComplete(true);
+    setTimeout(() => setAnalysisComplete(false), 2000);
+  };
+
   return (
     <div className="analysis-page">
-      <AnalysisControl />
-      <AnalysisHistory />
+      <AnalysisControl onAnalysisComplete={handleAnalysisComplete} />
+      <AnalysisHistory key={analysisComplete ? 'refresh' : 'normal'} />
     </div>
   );
 };
