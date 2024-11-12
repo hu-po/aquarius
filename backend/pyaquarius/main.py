2eb9e7c2d18bbb6e9455ee2e0e0634435464c24b
hu.po <hugo.p.cmu@gmail.com>
2024-11-12 10:03:11 -0600

    asd

--- backend/pyaquarius/main.py
+++ backend/pyaquarius/main.py
@@ -84,7 +84,7 @@ robot_client = RobotClient()
 
 SCAN_CAMERA_ID = int(os.getenv('SCAN_CAMERA_ID', '0'))
 SCAN_INTERVAL = int(os.getenv('SCAN_INTERVAL', '10'))
-SCAN_ENABLED = os.getenv('SCAN_ENABLED', 'true').lower() == 'true'
+SCAN_ENABLED = os.getenv('SCAN_ENABLED', 'false').lower() == 'true'
 SCAN_TRAJECTORIES = os.getenv('SCAN_TRAJECTORIES', 'a,b,c,d').split(',')
 SCAN_SLEEP_TIME = int(os.getenv('SCAN_SLEEP_TIME', '5'))
 
