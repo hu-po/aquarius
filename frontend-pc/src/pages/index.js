10b9eddc6f4c9a49e6fbb96ca3043b1dd23ade2a
hu.po <hugo.p.cmu@gmail.com>
2024-11-13 14:49:41 -0600

    asd

--- frontend-pc/src/pages/index.js
+++ frontend-pc/src/pages/index.js
@@ -2,3 +2,4 @@ export { default as StreamsPage } from './StreamsPage';
 export { default as RobotPage } from './RobotPage';
 export { default as AnalysisPage } from './AnalysisPage';
 export { default as InfoPage } from './InfoPage';
+export { default as HomePage } from './HomePage';
