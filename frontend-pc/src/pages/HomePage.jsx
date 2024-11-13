3678b6ce20bd936046323413f29b0ba5abc933b7
hu.po <hugo.p.cmu@gmail.com>
2024-11-13 14:47:22 -0600

    qwe

--- frontend-pc/src/pages/HomePage.jsx
+++ frontend-pc/src/pages/HomePage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import AnalysisHistory from '../components/AnalysisHistory';
+import ToggleScan from '../components/ToggleScan';
 
 export const HomePage = () => {
   return (
     <div className="home-page">
       <div className="home-content">
+        <div className="scan-control">
+          <ToggleScan />
+        </div>
         <AnalysisHistory />
       </div>
     </div>
