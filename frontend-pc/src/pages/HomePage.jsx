6e5fe6cb4363dc61a60d52174a4333ff24e5405a
hu.po <hugo.p.cmu@gmail.com>
2024-11-15 12:31:01 -0600

    asd

--- frontend-pc/src/pages/HomePage.jsx
+++ frontend-pc/src/pages/HomePage.jsx
@@ -4,13 +4,18 @@ import ToggleScan from '../components/ToggleScan';
 
 export const HomePage = () => {
   return (
-    <div className="home-page">
+    <div className="home-page" style={{ msOverflowStyle: 'none', scrollbarWidth: 'none' }}>
       <div className="home-content">
         <div className="scan-control">
           <ToggleScan />
         </div>
         <AnalysisHistory />
       </div>
+      <style>{`
+        .home-page::-webkit-scrollbar {
+          display: none;
+        }
+      `}</style>
     </div>
   );
 };
